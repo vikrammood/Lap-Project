c56f95290415223e16855d5fd1946b54b2b6c6ed
Medha Chirumarri <45756816+MedhaChirumarri@users.noreply.github.com>
2020-11-08 11:58:34 +0530

    sample.php with changes
    this file is for admitcard.php 
    <form action="sample.php">

---
+++ sample.php
@@ -0,0 +1,20 @@
+<?php
+extract($_POST);
+$conn = new mysqli("localhost:3306","root","root-password","student");
+extract($_POST);
+if(isset($save))
+{
+$sql = "SELECT id,name,roll FROM studentdetails WHERE email = '$email'";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+    echo "id: " . $row["id"]. " - Name: " . $row["name"]. " " . $row["roll"]. "<br>";
+  }
+} else {
+  echo "0 results";
+}
+}
+$conn->close();
+?>
