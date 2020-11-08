4acffb9f07b67fe9dc137e04f4277af2cd2bc337
b18083.kalyan <47269164+kalyanram01@users.noreply.github.com>
2020-11-08 10:26:41 +0530

    Update admitcard.php

--- admitcard.php
+++ admitcard.php
@@ -66,12 +66,13 @@ a {
     <h1>Register</h1>
     <p>Please fill in this form to register for exams.</p>
     <hr>
-	  <label for="name"><b>Name</b></label>
+    <label for="name"><b>Name</b></label>
     <label for="email"><b>Email</b></label> 
     <input type="text" placeholder="Enter Email" name="email" id="email" required>
     <label for="psw"><b>Password</b></label>
     <input type="password" placeholder="Enter Password" name="psw" id="psw" required>
     <hr>
+   <! it shows information by creating an account you have to agree the terms and conditions >
     <p>By creating an account you agree to our <a href="#">Terms & Privacy</a>.</p>
     <input type="submit" name="save" value="Submit"/>
   </div>
