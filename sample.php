c98d3dd6f48be2543db528362f00417f7cf944eb
Medha Chirumarri <45756816+MedhaChirumarri@users.noreply.github.com>
2020-11-08 21:20:40 +0530

    updated sample.php by making a table

--- sample.php
+++ sample.php
@@ -1,17 +1,58 @@
 <?php
 extract($_POST);
-$conn = new mysqli("localhost:3306","root","root-password","student");
+$conn = new mysqli("localhost:3306","root","Med09","student");
 extract($_POST);
 if(isset($save))
 {
-$sql = "SELECT id,name,roll FROM studentdetails WHERE email = '$email'";
+$sql = "SELECT id,name,roll,branch,dob,email FROM studentdetails WHERE email = '$email'";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
   // output data of each row--
+echo  "<table border='1'>
+
+<tr>
+
+<th>Id</th>
+
+<th>name</th>
+
+<th>branch</th>
+
+<th>dob</th>
+
+<th>
+email
+</th>
+
+</tr>";
   while($row = $result->fetch_assoc()) {
-    echo "id: " . $row["id"]. " - Name: " . $row["name"]. " " . $row["roll"]. "<br>";
-  }
+  //  echo " Name: " . $row["name"]. "<br> " ."Roll no:". $row["roll"]. "<br>"."Branch:".$row["branch"]."<br />";
+
+
+
+
+
+
+        {
+
+        echo "<tr>";
+
+        echo "<td>" . $row['name'] . "</td>";
+
+        echo "<td>" . $row['roll'] . "</td>";
+
+        echo "<td>" . $row['branch'] . "</td>";
+
+        echo "<td>" . $row['dob'] . "</td>";
+
+        echo "<td>".$row['email']."</td>";
+        echo "</tr>";
+
+        }
+
+      echo "</table>";
+        }
 } else {
   echo "0 results";
 }
