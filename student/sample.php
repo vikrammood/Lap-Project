f0329f4427e6f82419cf63791654204f83a0f866
Keerthi B18049 <68141360+keerthi62@users.noreply.github.com>
2020-11-16 19:48:28 +0530

    Update sample.php

--- student/sample.php
+++ student/sample.php
@@ -52,35 +52,10 @@ email
 }
 }
 $conn->close();
-
-$dir = "uploads";
-
-// Open a directory, and read its contents
-if (is_dir($dir)){
-  if ($dh = opendir($dir)){
-    while (($file = readdir($dh)) !== false){
-      if($file != "." && $file != ".."){
-            echo "<a href='/uploads/" . $file . "' download>" . $file . "</a><br>";
-        }
-    }
-    closedir($dh);
-  }
-}
 ?>
 
 <!DOCTYPE html>
 <html>
-<head>
-<style>
-a{
-	text-decoration: none;
-	color: blue;
-}
-a:hover{
-	text-decoration: underline;
-}
-</style>
-</head>
 <body>
 <button onclick="window.location.href='register.php'">A</button>
 <button onclick="window.location.href='register.php'">Sign Up</button>
