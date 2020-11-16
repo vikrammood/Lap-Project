d29d2ba6c0a111f61922b8be110ccc96ecc8d5a7
Keerthi B18049 <68141360+keerthi62@users.noreply.github.com>
2020-11-16 19:50:29 +0530

    Create sample_p.php

---
+++ princy/sample_p.php
@@ -0,0 +1,31 @@
+<?php
+$dir = "uploads";
+// Open a directory, and read its contents
+if (is_dir($dir)){
+  if ($dh = opendir($dir)){
+    while (($file = readdir($dh)) !== false){
+      if($file != "." && $file != ".."){
+            echo "<a href='/uploads/" . $file . "' download>" . $file . "</a><br>";
+        }
+    }
+    closedir($dh);
+  }
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+a{
+	text-decoration: none;
+	color: blue;
+}
+a:hover{
+	text-decoration: underline;
+}
+</style>
+</head>
+<body>
+</body>
+</html>
