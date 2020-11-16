109ae06de81de52b6d9a2b59a255ace0344f646f
Medha Chirumarri <45756816+MedhaChirumarri@users.noreply.github.com>
2020-11-16 14:11:32 +0530

    front page for registration for student/admin/principal

---
+++ front_page.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <form class=""enctype="multipart/form-data" action="register.php" method="post" target="_blank">
+        <input type="button" value="Register for student" onclick="window.location.href='register.php'" />
+        <input type="button" value="Register for admin" onclick="window.location.href='register_a.php'" />
+
+    </form>
+  </body>
+</html>
