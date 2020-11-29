2a411dd9194b0d58c895866222acaa9fcc72f96b
b18083.kalyan <47269164+kalyanram01@users.noreply.github.com>
2020-11-29 17:11:49 +0530

    Update sample_a.php

--- admin/sample_a.php
+++ admin/sample_a.php
@@ -4,10 +4,10 @@ echo;
 <html>
 <body>
 
-<form action="upload.php" method="post" enctype="multipart/form-data">
+<form action="upload.php" method="post" enctype="multipart/form-data"> <!--on form submission it redirects to upload.php-->
   Select image to upload:
-  <input type="file" name="fileToUpload" id="fileToUpload">
-  <input type="submit" value="Upload Image" name="submit">
+  <input type="file" name="fileToUpload" id="fileToUpload"> <!--upload pdf file or text file -->
+  <input type="submit" value="Upload Image" name="submit"> <!-- submit file -->
 </form>
 
 </body>
