a629c80f3acabd1f3b09ea79ecc46867a3253956
b18083.kalyan <47269164+kalyanram01@users.noreply.github.com>
2020-11-29 17:50:00 +0530

    Update login_p.php

--- princy/login_p.php
+++ princy/login_p.php
@@ -72,7 +72,7 @@ hr {
     <label for="email"><b>Email</b></label>
     <input type="text" placeholder="Enter Email" name="email" id="email" required>
     <label for="psw"><b>Password</b></label>
-    <input type="password" placeholder="Enter Password" name="psw" id="psw" required>
+    <input type="password" placeholder="Enter Password" name="psw" id="psw" required> 
     <hr>
     <input type="submit" class="loginbtn" name="save" value="Submit"/>
   </div>
