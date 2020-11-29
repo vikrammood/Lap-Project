ecd466181d45624d8703e1c50c9066a7e87edc2c
b18083.kalyan <47269164+kalyanram01@users.noreply.github.com>
2020-11-29 17:04:54 +0530

    Update sample.php

--- student/sample.php
+++ student/sample.php
@@ -1,14 +1,13 @@
 <?php
-extract($_POST);
-$conn = new mysqli("localhost:3306","root","Keerthi@2001","student");
-extract($_POST);
-if(isset($save))
+$conn = new mysqli("localhost:3306","root","Keerthi@2001","student");//create connection
+extract($_POST//imports post variables
+if(isset($save))//it checks whether we clicked the submit button or not
 {
-$sql = "SELECT id,name,roll,branch,dob,email FROM studentdetails WHERE email = '$email'";
+$sql = "SELECT id,name,roll,branch,dob,email FROM studentdetails WHERE email = '$email'"; //selects the specified columns where emial = entered email
 $result = $conn->query($sql);
 
-if ($result->num_rows > 0) {
-  // output data of each row--
+if ($result->num_rows > 0) { //if the entered email exists.
+  // admit card of student.--
 echo  "<table border='1'>
 
 <tr>
@@ -26,7 +25,7 @@ email
 </th>
 
 </tr>";
-  while($row = $result->fetch_assoc()) {
+  while($row = $result->fetch_assoc()) { //fetches all rows in result variable.
   //  echo " Name: " . $row["name"]. "<br> " ."Roll no:". $row["roll"]. "<br>"."Branch:".$row["branch"]."<br />";
         {
 
@@ -57,7 +56,7 @@ $conn->close();
 <!DOCTYPE html>
 <html>
 <body>
-<button onclick="window.location.href='register.php'">A</button>
-<button onclick="window.location.href='register.php'">Sign Up</button>
+<button onclick="window.location.href='register.php'">A</button> 
+<button onclick="window.location.href='register.php'">Sign Up</button> 
 </body>
 </html>
