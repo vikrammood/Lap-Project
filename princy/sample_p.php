216ac63304f43a58d171f1608c4a71168357a2a2
vikrammood <b18073@students.iitmandi.ac.in>
2020-11-30 00:10:16 +0530

    Update sample_p.php

--- princy/sample_p.php
+++ princy/sample_p.php
@@ -1,12 +1,12 @@
 <?php
-$dir = "uploads";
+$dir = "../uploads";
 // Open a directory, and read its contents
 if (is_dir($dir)){
   if ($dh = opendir($dir)){
     while (($file = readdir($dh)) !== false){ 
 	    //loop through the files, skipping . and .., and recursing if necessary
       if($file != "." && $file != ".."){
-            echo "<a href='/uploads/" . $file . "' download>" . $file . "</a><br>";
+            echo "<a href='../uploads/" . $file . "' download>" . $file . "</a><br>";
         }
     }
     closedir($dh);
