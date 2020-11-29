b76587aa5cdbed6607cb0b91f7302d76f0050f6f
b18083.kalyan <47269164+kalyanram01@users.noreply.github.com>
2020-11-29 16:49:26 +0530

    Update registration-form.php

--- student/registration-form.php
+++ student/registration-form.php
@@ -1,29 +1,29 @@
 <?php
 //create connection.
 $conn = new mysqli("localhost:3306","root","Keerthi@2001","student");
-extract($_POST);
-if(isset($save))
+extract($_POST); //imports post variables
+if(isset($save)) //it checks whether we clicked the submit button or not.
 {
 $dob=$yy."-".$mm."--".$dd; 
-$sql = "SELECT * FROM studentdetails";
-$count = $conn->query($sql)->num_rows;
-$sql = "SELECT * FROM studentdetails where email = '$e'";
+$sql = "SELECT * FROM studentdetails"; //selecting all rows from studentdetails.
+$count = $conn->query($sql)->num_rows; //counts number of rows in student details
+$sql = "SELECT * FROM studentdetails where email = '$e'"; //selecting the row where email = entered email.
 $result = $conn->query($sql);
-if ($result->num_rows > 0) {
-$msg = "<font color='red'>".ucfirst($e)."already exists choose another email</font>";
+if ($result->num_rows > 0) { //if the entered email already exists.
+$msg = "<font color='red'>".ucfirst($e)."already exists choose another email</font>"; 
 }
 else
 {
-$query="INSERT INTO studentdetails VALUES ($count+1,'$n','$r','$b','$e','$g','$p','$dob',now())";
+$query="INSERT INTO studentdetails VALUES ($count+1,'$n','$r','$b','$e','$g','$p','$dob',now())"; //inserting the details in specified order.
 
-if ($conn->query($query) === TRUE) {
-  header("Location: login.php");
+if ($conn->query($query) === TRUE) { //if details are inserted succesfully 
+  header("Location: login.php"); //redirect to login page.
 } else {
-  echo "Error: " . $query . "<br>" . $conn->error;
+  echo "Error: " . $query . "<br>" . $conn->error; //if not insterted succesfully it shows error
 }
 }
 }
-$conn->close();
+$conn->close(); //close connection.
 ?>
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
@@ -63,42 +63,42 @@ label{
 <form method="post" enctype="multipart/form-data">
 <table width="500" height="300" border="1"> 
 <?php  
-  if(isset($msg)){
-  echo $msg;
+  if(isset($msg)){ //it shows the error msg .
+  echo $msg; 
   }
 ?>
   <tr>
-    <th width="150">Enter your Name</th>
-    <td width="200">
-	<input type="text" placeholder="your first name" name="n" pattern="[a-z A-Z]*" required /></td>
+    <th width="150">Enter your Name</th> 
+    <td width="200"> 
+	<input type="text" placeholder="your first name" name="n" pattern="[a-z A-Z]*" required /></td>  <!--enter your name-->
   </tr>
   <tr>
     <th>Enter your Roll no.</th>
-    <td><input type="text" name="r"/></td>
+    <td><input type="text" name="r"/></td> <!--enter your rollno -->
   </tr>
   <tr>
     <th>Enter your Branch</th>
-    <td><input type="text" name="b"/></td>
+    <td><input type="text" name="b"/></td> <!--enter your Branch -->
   </tr>
   <tr>
     <th>Enter your Email</th>
-    <td><input type="email" id="email" name="e"/></td>
+    <td><input type="email" id="email" name="e"/></td> <!--enter your email-->
   </tr>
   <tr>
-    <th height="23">Select your Gender</th>
+    <th height="23">Select your Gender</th> 
     <td style="vertical-align: top;">
   <input type="radio" name="g" value="m"/><label>Male</label>
-	<input type="radio" name="g" value="f"/><label>Female</label>
+	<input type="radio" name="g" value="f"/><label>Female</label> <!-- select gender -->
   </td>
   </tr>
   <tr>
     <th>Enter your Password</th>
-    <td><input type="password" name="p"/></td>
+    <td><input type="password" name="p"/></td> <!--enter password -->
   </tr>
   <tr>
     <th>Select your DOB</th>
     <td>
-		<select name="mm">
+		<select name="mm"> <!-- select month -->
 			<option value="">Month</option>
 			<?php 
 			for($i=1;$i<=12;$i++)
@@ -107,7 +107,7 @@ label{
 			}
 			?>
 		</select>
-		<select name="dd">
+		<select name="dd"> <!-- select date -->
 			<option value="">Date</option>
 			<?php 
 			for($i=1;$i<=31;$i++)
@@ -116,7 +116,7 @@ label{
 			}
 			?>
 		</select>
-		<select name="yy">
+		<select name="yy"> <!-- select year -->
 			<option value="">Year</option>
 			<?php 
 			for($i=1900;$i<=2015;$i++)
@@ -129,12 +129,12 @@ label{
   </tr>
 </table>
 <div style="margin-top: 10px">
-  <input type="submit" name="save" class="sbtn" value="Register Me"/>
-  <input type="reset" value="Reset"/>
+  <input type="submit" name="save" class="sbtn" value="Register Me"/> <!-- submit details -->
+  <input type="reset" value="Reset"/> <!-- reset details -->
 </div>
 </form>
 <br>
-<input type="button" value="Login Me" onclick="window.location.href='login.php'" />
+<input type="button" value="Login Me" onclick="window.location.href='login.php'" /> <!-- redirects to login page. -->
 </div>
 </body>
 </html>
